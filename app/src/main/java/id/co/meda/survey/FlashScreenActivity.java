ba58bcaa8a81eaffd603c9e668d32327a475ceac
I Putu Medagia A <iputu.medagia@gmail.com>
2016-04-26 12:04:32 +0700

    first

---
+++ app/src/main/java/id/co/meda/survey/FlashScreenActivity.java
@@ -0,0 +1,50 @@
+package id.co.meda.survey;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Handler;
+
+
+
+public class FlashScreenActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(FlashScreenActivity.this, UserActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_flash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/id/co/meda/survey/config/UrlConfig.java
@@ -0,0 +1,10 @@
+package id.co.meda.survey.config;
+
+/**
+ * Created by I Putu Medagia A on 4/15/2016.
+ */
+public class UrlConfig {
+
+    public static String URL_LOGIN = "http://192.168.56.102/login.php";
+    public static String URL_REGISTRATION = "http://192.168.56.102/registration.php";
+}
