03f7a3baad1ff352c1b59e336976cb723da34c36
umarfarisi <pharese@gmail.com>
2016-04-27 23:14:19 -0800

    menambah model : User, Product, dan Barcode

---
+++ app/src/main/java/id/co/meda/survey/model/Barcode.java
@@ -0,0 +1,7 @@
+package id.co.meda.survey.model;
+
+/**
+ * Created by Faris on 27/04/2016.
+ */
+public class Barcode {
+}
---
+++ app/src/main/java/id/co/meda/survey/model/Product.java
@@ -0,0 +1,61 @@
+package id.co.meda.survey.model;
+
+/**
+ * Created by Faris on 27/04/2016.
+ */
+public class Product {
+
+    private String name;
+    private String category;
+    private String description;
+    private String photo;
+    private Barcode barcode;
+
+    public Product(String name, String category, String description, String photo, Barcode barcode) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.photo = photo;
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Barcode getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(Barcode barcode) {
+        this.barcode = barcode;
+    }
+}
