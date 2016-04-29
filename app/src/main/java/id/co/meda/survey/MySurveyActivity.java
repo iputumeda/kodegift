f918deec03674adc282e171980238da134b59aec
umarfarisi <pharese@gmail.com>
2016-04-29 10:39:38 -0800

    menmbahkan kelas AsynTask untuk memproses database

--- app/src/main/java/id/co/meda/survey/MySurveyActivity.java
+++ app/src/main/java/id/co/meda/survey/MySurveyActivity.java
@@ -3,6 +3,7 @@ package id.co.meda.survey;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CursorAdapter;
@@ -14,20 +15,17 @@ import id.co.meda.survey.database.VoucherDatabase;
 
 public class MySurveyActivity extends ListActivity {
 
+    ListView listView;
     private SurveyDatabase database;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_survey);
 
-        ListView listView = getListView();
-        database = new SurveyDatabase(this);
-        Cursor cursor = database.queryProducts();
-        CursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor,
-                new String[]{SurveyDatabase.SurveyDatabaseHelper.NAME_COLUMN},
-                new int[]{android.R.id.text1},0);
-        listView.setAdapter(adapter);
+        SurveyAsyncTask task = new SurveyAsyncTask();
+        task.execute();
 
     }
 
@@ -42,5 +40,29 @@ public class MySurveyActivity extends ListActivity {
     protected void onDestroy() {
         super.onDestroy();
         database.close();
+        cursor.close();
+    }
+
+    private class SurveyAsyncTask extends AsyncTask<Void, String, Cursor>{
+
+        @Override
+        protected void onPreExecute() {
+            listView = getListView();
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            database = new SurveyDatabase(MySurveyActivity.this);
+            cursor = database.queryProducts();
+            return cursor;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            CursorAdapter adapter = new SimpleCursorAdapter(MySurveyActivity.this, android.R.layout.simple_list_item_1, cursor,
+                    new String[]{SurveyDatabase.SurveyDatabaseHelper.NAME_COLUMN},
+                    new int[]{android.R.id.text1},0);
+            listView.setAdapter(adapter);
+        }
     }
 }
--- app/src/main/java/id/co/meda/survey/MyVoucherActivity.java
+++ app/src/main/java/id/co/meda/survey/MyVoucherActivity.java
@@ -1,7 +1,9 @@
 package id.co.meda.survey;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
@@ -11,26 +13,49 @@ import id.co.meda.survey.database.VoucherDatabase;
 
 public class MyVoucherActivity extends ListActivity {
 
-    private VoucherDatabase database;
     ListView listView;
+    private VoucherDatabase database;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_voucher);
-        listView = getListView();
-        database = new VoucherDatabase(MyVoucherActivity.this);
-        Cursor cursor = database.queryVoucher();
-        CursorAdapter adapter = new SimpleCursorAdapter(MyVoucherActivity.this, R.layout.item_voucher, cursor,
-                new String[]{VoucherDatabase.VoucherDatabaseHelper.NAME_COLUMN,VoucherDatabase.VoucherDatabaseHelper.VOUCHER_COLUMN},
-                new int[]{R.id.productName_tv_iv, R.id.voucher_tv_iv},0);
-        listView.setAdapter(adapter);
+
+        VoucherAsyncTask task = new VoucherAsyncTask();
+        task.execute();
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        cursor.close();
         database.close();
+    }
+
+    private class VoucherAsyncTask extends AsyncTask<Void, String, Cursor>{
+
+        @Override
+        protected void onPreExecute() {
+            listView = getListView();
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... contexts) {
+            database = new VoucherDatabase(MyVoucherActivity.this);
+            cursor =  database.queryVoucher();
+            return cursor;
+        }
+
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            CursorAdapter adapter = new SimpleCursorAdapter(MyVoucherActivity.this, R.layout.item_voucher, cursor,
+                    new String[]{VoucherDatabase.VoucherDatabaseHelper.NAME_COLUMN,VoucherDatabase.VoucherDatabaseHelper.VOUCHER_COLUMN},
+                    new int[]{R.id.productName_tv_iv, R.id.voucher_tv_iv},0);
+            listView.setAdapter(adapter);
+        }
     }
 
 }
