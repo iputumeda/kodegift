be8884a125d6d33467c33fed13d08e8d452b59b4
umarfarisi <pharese@gmail.com>
2016-04-29 20:22:26 -0800

    mengganti UserActivity menjadi LoginActivity

--- app/src/main/java/id/co/meda/survey/LoginActivity.java
+++ app/src/main/java/id/co/meda/survey/LoginActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 
 
 
-public class UserActivity extends Activity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
