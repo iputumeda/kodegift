452864160a3bc292ef04689001801e5253589a9b
umarfarisi <pharese@gmail.com>
2016-05-03 15:15:06 -0800

    menambahkan feature barcode

--- app/src/main/java/id/co/meda/survey/SurveyActivity.java
+++ app/src/main/java/id/co/meda/survey/SurveyActivity.java
@@ -1,18 +1,24 @@
 package id.co.meda.survey;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import java.io.ByteArrayOutputStream;
 
 import id.co.meda.survey.database.SurveyDatabase;
-import id.co.meda.survey.database.VoucherDatabase;
+import id.co.meda.survey.model.Barcode;
 import id.co.meda.survey.model.Product;
 import id.co.meda.survey.model.Voucher;
 
@@ -20,11 +26,14 @@ public class SurveyActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private byte[] imageToDatabase;
+    private String contents;
+    private String formatName;
+    private String productNameString;
+    private String productCategoryString;
+    private String productDescriptionString;
     EditText productName;
     EditText productCategory;
     EditText productDescription;
-    SurveyDatabase databaseSurvey;
-    VoucherDatabase databaseVoucher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,61 +48,87 @@ public class SurveyActivity extends AppCompatActivity {
         productName = (EditText) findViewById(R.id.productName_et_as);
         productCategory = (EditText) findViewById(R.id.productCategory_et_as);
         productDescription = (EditText) findViewById(R.id.productDescription_et_as);
-        databaseSurvey = new SurveyDatabase(this);
-        databaseVoucher = new VoucherDatabase(this);
         imageToDatabase = null;
     }
 
     public void onClickBarcodeProduct(View view) {
-
-        //TODO beralih ke pengambilan barcode
-
+        new IntentIntegrator(this).initiateScan();
     }
 
     public void onClickPhotoProduct(View view) {
-
         Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePhotoIntent.resolveActivity(getPackageManager()) != null){
             startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
         }
-
     }
 
     public void onClickSurveyProduct(View view) {
+        productNameString = productName.getText().toString();
+        productCategoryString = productCategory.getText().toString();
+        productDescriptionString = productDescription.getText().toString();
 
-        String productNameString = productName.getText().toString();
-        String productCategoryString = productCategory.getText().toString();
-        String productDescriptionString = productDescription.getText().toString();
-
-        Product product = new Product(productNameString, productCategoryString, productDescriptionString,imageToDatabase,null);
-        databaseSurvey.insertProduct(product);
-
-        Voucher voucher = new Voucher(productNameString, 100);
-        databaseVoucher.insertVoucher(voucher);
-
-        Toast.makeText(this, "survey is saved", Toast.LENGTH_SHORT).show();
-        finish();
-
+        SurveyTask task = new SurveyTask();
+        task.execute();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){ //menerima hasil photo
+
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+            if(imageBitmap != null) {
+                //konvert Bitmap ke array of bytes
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+                imageToDatabase = stream.toByteArray();
+                Log.e("SEVTRIMAMEN", "PHOTO IS GOTTEN");
+            }else{
+                Log.e("SEVTRIMAMEN", "PHOTO IS NOT GOTTEN");
+            }
 
-            //konvert Bitmap ke array of bytes
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            imageBitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
-            imageToDatabase = stream.toByteArray();
+        }else if (requestCode == IntentIntegrator.REQUEST_CODE) { // menerima hasil barcode
+
+            Log.e("SEVTRIMAMEN", "REQUEST CODE TRUE FOR GETTING BARCODE");
+            IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if (scanResult != null) {
+                contents = scanResult.getContents();
+                formatName = scanResult.getFormatName();
+                Log.e("SEVTRIMAMEN", "SCANRESULT IS NOT NULL, CONTENT AND FORMATNAME IS GOTTEN");
+            } else {
+                Log.e("SEVTRIMAMEN", "SCANRESULT IS NULL, CONTENT AND FORMATNAME IS NOT GOTTEN");
+            }
 
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        databaseSurvey.close();
-        databaseVoucher.close();
+    private class SurveyTask extends AsyncTask<Void, Void, Boolean>{
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                SurveyDatabase databaseSurvey = new SurveyDatabase(SurveyActivity.this);
+                Product product = new Product(productNameString, productCategoryString, productDescriptionString, imageToDatabase, new Barcode(contents, formatName));
+                databaseSurvey.insertProduct(product);
+                databaseSurvey.close();
+                Log.e("SEVTRIMAMEN", "SURVEY IS SAVED");
+                return true;
+            }catch (SQLiteException e){
+                Log.e("SEVTRIMAMEN", "SURVEY IS NOTSAVED, PESAN: "+e.getMessage());
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isSaved) {
+            if(isSaved) {
+                Toast.makeText(SurveyActivity.this, "survey is saved", Toast.LENGTH_SHORT).show();
+            }
+            finish();
+        }
     }
+
+
+
 }
