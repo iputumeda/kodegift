8acb59a789c36b000e1c04908a1afdc5e26deceb
umarfarisi <pharese@gmail.com>
2016-05-03 15:16:18 -0800

    memperbaiki id yg dipassing ke Activity Survey Detail

--- app/src/main/java/id/co/meda/survey/SurveyDetailActivity.java
+++ app/src/main/java/id/co/meda/survey/SurveyDetailActivity.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +25,12 @@ public class SurveyDetailActivity extends AppCompatActivity {
 
     private static final String PRODUCT = "CURRENT PRODUCT";
     public static final String ID = "CURRENT_ID";
+    private SurveyDatabase database;
+    private Cursor cursor;
+    TextView productTitle;
+    ImageView productPhoto;
+    TextView productCategory;
+    TextView productDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +38,19 @@ public class SurveyDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_survey_detail);
 
         DetailTask task = new DetailTask();
-        task.execute(getIntent().getIntExtra(ID, -1));
+        int id = getIntent().getIntExtra(ID, 0);
+        Log.e("SEVTRIMAMEN", "ID: "+id);
+        task.execute(id);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        database.close();
     }
 
     private class DetailTask extends AsyncTask<Integer, Void, Product>{
-
-        TextView productTitle;
-        ImageView productPhoto;
-        TextView productCategory;
-        TextView productDescription;
 
         @Override
         protected void onPreExecute() {
@@ -52,8 +63,9 @@ public class SurveyDetailActivity extends AppCompatActivity {
         @Override
         protected Product doInBackground(Integer... params) {
             try {
-                SurveyDatabase database = new SurveyDatabase(SurveyDetailActivity.this);
-                Cursor cursor = database.queryProducts(params[0]);
+                database = new SurveyDatabase(SurveyDetailActivity.this);
+                cursor = database.queryProducts(params[0]);
+                Log.e("SEVTRIMAMEN", "ROW: "+cursor.getCount());
                 cursor.moveToFirst();
                 String name = cursor.getString(
                         cursor.getColumnIndex(SurveyDatabase.SurveyDatabaseHelper.NAME_COLUMN)
@@ -67,9 +79,8 @@ public class SurveyDetailActivity extends AppCompatActivity {
                 byte[] photo = cursor.getBlob(
                         cursor.getColumnIndex(SurveyDatabase.SurveyDatabaseHelper.PHOTO_COLUMN)
                 );
-                cursor.close();
-                database.close();
-                return new Product(name, category, description, photo, null);
+                Product product = new Product(name, category, description, photo, null);
+                return product;
             }catch (SQLiteException e){
                 return null;
             }
